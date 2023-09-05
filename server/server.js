bc72c8caaad0f4e6ecfe05ea2313a6bd602688f1
Anthony <andeng2@berkeley.edu>
2023-09-05 01:20:04 -0700

    base design:

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+
+app.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+      redirectUri: process.env.REDIRECT_URI,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken,
+    })
+  
+    spotifyApi
+      .refreshAccessToken()
+      .then(data => {
+        res.json({
+          accessToken: data.body.accessToken,
+          expiresIn: data.body.expiresIn,
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        res.sendStatus(400)
+      })
+})
